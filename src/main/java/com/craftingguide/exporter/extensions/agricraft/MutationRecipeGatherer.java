c37278a05b7ff5ea705b178efeed6b9f97a78268
Andrew Miner <andrewminer@mac.com>
2016-12-27 15:33:44 -0800

    Add Agricraft

--- src/main/java/com/craftingguide/exporter/extensions/agricraft/MutationRecipeGatherer.java
+++ src/main/java/com/craftingguide/exporter/extensions/agricraft/MutationRecipeGatherer.java
@@ -1,17 +1,24 @@
 package com.craftingguide.exporter.extensions.agricraft;
 
+import com.InfinityRaider.AgriCraft.api.v1.BlockWithMeta;
+import com.InfinityRaider.AgriCraft.api.v1.IGrowthRequirement;
 import com.InfinityRaider.AgriCraft.api.v1.IMutation;
+import com.InfinityRaider.AgriCraft.farming.CropPlantHandler;
 import com.InfinityRaider.AgriCraft.farming.mutation.MutationHandler;
 import com.craftingguide.exporter.Gatherer;
 import com.craftingguide.exporter.models.ItemModel;
 import com.craftingguide.exporter.models.ItemStackModel;
 import com.craftingguide.exporter.models.ModPackModel;
 import com.craftingguide.exporter.models.RecipeModel;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class MutationRecipeGatherer extends Gatherer {
 
     public void gather(ModPackModel modPack) {
+        ItemModel crops = modPack.getItem(CROPS_ID);
+        ItemModel dirt = modPack.getItem(DIRT_ID);
+
         for (IMutation rawMutation : MutationHandler.getInstance().getMutations()) {
             ItemModel outputItem = modPack.getItem(rawMutation.getResult());
             RecipeModel recipe = new RecipeModel(new ItemStackModel(outputItem, 1));
@@ -26,13 +33,47 @@ public class MutationRecipeGatherer extends Gatherer {
                 index++;
             }
 
+            recipe.setInputAt(0, 1, new ItemStackModel(crops, 2));
+            recipe.addExtra(new ItemStackModel(crops, 1));
+
+            IGrowthRequirement growth = CropPlantHandler.getGrowthRequirement(outputItem.getRawItemStack());
+
+            ItemModel soil = asItem(growth.getSoil(), modPack);
+            soil = (soil != null) ? soil : dirt;
+            if (soil != null) {
+                recipe.setInputAt(1, 1, new ItemStackModel(soil, 1));
+                recipe.addExtra(new ItemStackModel(soil, 1));
+            }
+
+            ItemModel required = asItem(growth.getRequiredBlock(), modPack);
+            if (required != null) {
+                recipe.setInputAt(2, 1, new ItemStackModel(required, 1));
+                recipe.addExtra(new ItemStackModel(required, 1));
+            }
+
             modPack.addRecipe(recipe);
         }
+    }
+
+    // Private Class Methods ///////////////////////////////////////////////////////////////////////////////////////////
+
+    private ItemModel asItem(BlockWithMeta rawBlock, ModPackModel modPack) {
+        if (rawBlock == null) return null;
+
+        Item rawItem = Item.getItemFromBlock(rawBlock.getBlock());
+        if (rawItem == null) return null;
+
+        ItemStack rawItemStack = new ItemStack(rawItem, 1, rawBlock.getMeta());
+        return modPack.getItem(rawItemStack);
     }
 
     // Private Class Properties ////////////////////////////////////////////////////////////////////////////////////////
 
     private int[] COL = { 0, 2, 1, 0, 2, 1, 0, 2, 1 };
 
-    private int[] ROW = { 1, 1, 1, 0, 0, 0, 2, 2, 2 };
+    private String CROPS_ID = "AgriCraft:cropsItem";
+
+    private String DIRT_ID = "minecraft:dirt";
+
+    private int[] ROW = { 0, 0, 0, 1, 1, 1, 2, 2, 2 };
 }
