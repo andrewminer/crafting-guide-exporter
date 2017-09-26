be9ab5b86fa781f20051732ea44daeb0d7d0517a
coehlrich <charlieloehlrich@gmail.com>
2017-09-26 18:57:24 +1300

    fix orange portal guns from the portal gun mod not being gotten

--- src/main/java/com/craftingguide/exporter/extensions/minecraft/ItemGatherer.java
+++ src/main/java/com/craftingguide/exporter/extensions/minecraft/ItemGatherer.java
@@ -7,10 +7,13 @@ import com.craftingguide.exporter.models.ModPackModel;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
+import codechicken.nei.guihook.GuiContainerManager;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.registry.GameData;
 
@@ -18,7 +21,7 @@ public class ItemGatherer extends Gatherer {
 
     @Override
     public void gather(ModPackModel modPack) {
-        Set<String> ids = (Set<String>) Item.itemRegistry.getKeys();
+    	Set<String> ids = (Set<String>) Item.itemRegistry.getKeys();
         for (String id : ids) {
             Item item = (Item) Item.itemRegistry.getObject(id);
 
@@ -34,6 +37,25 @@ public class ItemGatherer extends Gatherer {
             if (item.getHasSubtypes()) {
                 ArrayList<ItemStack> subItemStacks = new ArrayList<ItemStack>();
                 item.getSubItems(item, null, subItemStacks);
+                
+                if (subItemStacks.isEmpty()) {
+                	// The following has been borrowed from ChickenBones' NEI code at http://bit.ly/2fNyMLI
+                    // BEGIN NEI
+                	
+                	HashSet<String> damageIconSet = new HashSet<String>();
+                    for (int damage = 0; damage < 16; damage++) {
+                    	ItemStack itemstack = new ItemStack(item, 1, damage);
+                        IIcon icon = item.getIconIndex(itemstack);
+                        String name = GuiContainerManager.concatenatedDisplayName(itemstack, false);
+                        String s = name + "@" + (icon == null ? 0 : icon.hashCode());
+                        if (!damageIconSet.contains(s)) {
+                        	damageIconSet.add(s);
+                            subItemStacks.add(itemstack);
+                        }
+                    }
+                    
+                    // END NEI
+                }
 
                 for (ItemStack stack : subItemStacks) {
                     String subTypeId = id + ":" + stack.getItemDamage();
