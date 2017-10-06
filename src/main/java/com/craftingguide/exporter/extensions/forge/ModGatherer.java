376ea165d3dd181cae65a7a2e7f2eb1ac7c602d7
coehlrich <charlieloehlrich@gmail.com>
2017-10-07 08:58:02 +1300

    make it set the iconic block to something from the mod (atleast its better than setting it to stone)

--- src/main/java/com/craftingguide/exporter/extensions/forge/ModGatherer.java
+++ src/main/java/com/craftingguide/exporter/extensions/forge/ModGatherer.java
@@ -1,11 +1,14 @@
 package com.craftingguide.exporter.extensions.forge;
 
 import com.craftingguide.exporter.Gatherer;
+import com.craftingguide.exporter.models.ItemModel;
 import com.craftingguide.exporter.models.ModModel;
 import com.craftingguide.exporter.models.ModPackModel;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.ModContainer;
-import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.registry.GameData;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 public class ModGatherer extends Gatherer {
@@ -22,7 +25,17 @@ public class ModGatherer extends Gatherer {
             
             mod.setAuthor(mod.getRawMod().getMetadata().getAuthorList());
             mod.setDescription(mod.getRawMod().getMetadata().description);
-            mod.setIconicBlock(modPack.getItem(new ItemStack(Blocks.stone)));
+            ItemModel block = null;
+            for (Object itemObject : GameData.getItemRegistry()) {
+                Item item = (Item) itemObject;
+                if (GameData.getItemRegistry().getNameForObject(item).indexOf(rawMod.getModId()) == 0) {
+                    if (item instanceof ItemBlock) {
+                        block = new ItemModel(GameData.getItemRegistry().getNameForObject(item), new ItemStack(item));
+                        break;
+                    }
+                }
+            }
+            mod.setIconicBlock(block);
             mod.setUrl(mod.getUrl());
             mod.setEnabled(true);
             modPack.addMod(mod);
